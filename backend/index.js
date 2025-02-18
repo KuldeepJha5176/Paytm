71409b63d286c10c4e584d055e77ce7c831882b2
KuldeepJha5176 <ranacjha@gmail.com>
2025-02-18 23:06:43 +0530

    paytm project initiated

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,17 @@
 const express = require("express");
+const mainRouter = require("./routes/index");
+const cors = require("cors");
+const app = express();
+const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
+
+
+app.use("/api/v1", mainRouter)
+
+
+app.listen(PORT,
+    console.log("server is listening on PORT", PORT)
+)
 
